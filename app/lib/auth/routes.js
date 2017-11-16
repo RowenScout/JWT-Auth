ee51d8c456af9e79a465d4738d3bc17f07342480
Rowen Scout <rowen.scout@gmail.com>
2017-11-15 20:20:54 -0800

    added routes scaffold and some error handling.

--- app/lib/auth/routes.js
+++ app/lib/auth/routes.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const express = require('express');
+const parser = require('body-parser');
+const User = require('../model/user.js');
+const router = module.exports = express.Router();
+
 //sign up
-//req, res, next
+router.post('/users', (req, res, next) => {
+  if( !req.body.username) return new Error(error)
+  if( !req.body.password) return new Error(error)
+
+  new User(req.body).save();
+  .then(res.send('success message'))
+  .catch(err => next({err}))
+})
+
 //verify that req has username
 //verify that req has password
 
@@ -14,13 +27,43 @@
 
 //send res with user that was just created
 
-
 //delete
-
-//config
-
-//login
-
-//logout
+router.delete('/users:id', (req, res, next) => {
+  if(!req.params.id) return new Error('no id was specified.')
+  User.remove({_id:req.params.id})
+  .then(res.send('success message'))
+  .catch(err => next({error:err, status:400}))
+})
 
 //update
+router.put('/users/:id', (req, res, next) => {
+  if(!req.params.id) return new Error('no id was specified.')
+  User.findOneAndUpdate(_id:req.params.id}, req.body)
+  .then(res.send('success message'))
+  .catch(err => next({error:err, status:404}))
+})
+
+//config
+//???  User.findOneAndUpdate(_id:req.params.id}, req.body)
+router.verb('string' (req, res, next) => {
+  //sets up server?
+  //establish defaults using config object.
+})
+
+//login
+router.put('/users/:id'(req, res, next) => {
+  if(!req.params.id) return new Error('no id was specified.')
+  //update uuids
+  //send success message
+  //catch error
+
+})
+
+//logout
+router.put('/users/:id'(req, res, next) => {
+  if(!req.params.id) return new Error('no id was specified.')
+  //update uuids
+  //send success message
+  //catch error
+
+})
