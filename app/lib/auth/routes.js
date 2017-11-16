bf498a9fcbce99c2b4f722a88713b1250c7d7dce
Rowen Scout <rowen.scout@gmail.com>
2017-11-15 19:59:20 -0800

    added further scaffolding and some comments on what each file should do.

--- app/lib/auth/routes.js
+++ app/lib/auth/routes.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//sign up
+//req, res, next
+//verify that req has username
+//verify that req has password
+
+//pass req into user constructor
+
+//now this is where I get lost.
+
+//does the user model have code that update the user in mongodb,
+//or does the route handle that?
+
+//send res with user that was just created
+
+
+//delete
+
+//config
+
+//login
+
+//logout
+
+//update
