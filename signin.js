d82a7b58930e0cb230c07926b5ca895dc0e7a547
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-11-18 11:53:36 -0800

    sign in update

--- signin.js
+++ signin.js
@@ -7,24 +7,36 @@ const router = module.exports = express.Router();
 module.exports = (req, res, next) => {
     console.log("Signin Testing");
 
+      let base64 = authHeader = req.headers.authorization;
+      let base64Buffer = new Buffer(base64, 'base64');
+      let stringHeader = base64Buffer.toString();
+      let authArray = stringHeader.split(':');
+      let authObject = {username: authArray[0], password:authArray[1]};
+
+
 //take username/password from header
 
-router.get('/signin', jwtAuth().signin, (req,res,next))
+router.get('/signin', jwtAuth().signin, (req,res,next)) => {
 
 //autheniticate
-User.findOne({username: req.user.username})
+User.findOne({authObject})
+// {username: req.user.username
 // or authObject
 .then(user => {
 req.user = {
         //authenticate
-        if (!user) next (403)
+        if (!user) next (403);
+        if (!user.username) next (404);
+        if (!user.password) next (404);
         //message
+        if (!)
         //token
+
         //user
     })
 
 //if correct, generate just token
 //use uuid of user
 
-
+}
 };
