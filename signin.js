f1b452001cac0376c266a28824d0fa8fe85b1438
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-11-18 12:44:49 -0800

    comparing user in db

--- signin.js
+++ signin.js
@@ -11,29 +11,37 @@ module.exports = (req, res, next) => {
       let base64Buffer = new Buffer(base64, 'base64');
       let stringHeader = base64Buffer.toString();
       let authArray = stringHeader.split(':');
-      let authObject = {username: authArray[0], password:authArray[1]};
-
+      let authObject = {
+        username: authArray[0],
+        password:authArray[1]
+      };
 
 //take username/password from header
-
 router.get('/signin', jwtAuth().signin, (req,res,next)) => {
 
 //autheniticate
-User.findOne({authObject})
+// User.findOne({authObject})
 // {username: req.user.username
 // or authObject
-.then(user => {
-req.user = {
-        //authenticate
-        if (!user) next (403);
-        if (!user.username) next (404);
-        if (!user.password) next (404);
-        //message
-        if (!)
-        //token
-
-        //user
-    })
+User.findOne({username: authObject['username']}).then(response => {
+  if (response) {
+    req.user = {
+      //authenticate
+      if (!user) next (err: 403);
+      if (!user.username) next (err: 404);
+      if (!user.password) next (err: 404);
+      next();
+      //message
+      if (req.user = req.user.db) {
+        //if the user doesn't match the user in the db
+      return message: `Welcome + ${req.user.username}!`
+    } else {
+      return err: 404
+    }
+      //token
+      
+      //user
+  })
 
 //if correct, generate just token
 //use uuid of user
