82ddc8aa64d7de500f8d5409fa309d3f3ba357da
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-11-18 16:06:36 -0800

    sign in and user clean up

--- signin.js
+++ signin.js
@@ -3,12 +3,9 @@
 const jwt = require('jsonwebtoken');
 const User = require('./lib/user.js');
 const bcrypt = require('bluebird').promisifyAll(require('bcrypt'));
-// require('dotenv').config();
-
 
 module.exports = (req, res, next) => {
-console.log('Signing Testing');
-  req.user = {};
+  req.user = req.user || {};
       let authHeader = req.headers.authorization;
       if (!authHeader) return next(new Error('No auth Header'));
       let base64 = authHeader.split('Basic ')[1];
@@ -27,17 +24,17 @@ User.findOne({username: authObject['username']}).then(response => {
      function (res, err){
        if (res) {
          req.user.authenticated = res;
-         //make token
            req.user.token = jwt.sign({id: response.uuid}, process.env.SECRET || 'change this');
-           console.log(req.user);
           next();
        } else {
-         req.user.authenticated = res;
+         req.user.authenticated = false;
+         next();
        }
      });
     next();
   } else {
-    console.log('err');
+    req.user.authenticated = false;
+    next();
   }
 
 });
