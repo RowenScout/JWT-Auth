cf4693559b95d761265a638db1ab414d9b9ea894
David Boucher <david.boucher@ookla.com>
2017-11-26 12:14:15 -0800

    Added master to kelati's work

--- logout.js
+++ logout.js
@@ -2,29 +2,19 @@ const User = require('./lib/user.js');
 const jwt = require('jsonwebtoken');
 const uuid = require('uuid');
 
+const getHeader = require('./lib/getHeader.js');
+
 module.exports = (req, res, next) => {
-  console.log("Logout Testing");
   req.user = req.user || {};
-//invalidate token
-//set authorication from true to false
-//get bearer token from req.user
-//change uuid
+  let authHeader = getHeader(req, next);
 
-  // console.log(req.headers);
+  let verified = jwt.verify(authHeader.token, process.env.SECRET || 'change this')
 
-  req.headers = req.headers || {};
-  let bearer = req.headers.authorization;
-  if (!bearer) return next(new Error ('Bearer invalid'));
-  let token = bearer.split('Bearer ')[1];
-    // console.log(token);
-  let verified = jwt.verify(token, process.env.SECRET || 'change this')
-    // console.log(verified);
-  // search db for matches verfied.id
   User.findOne({uuid: verified['id']}).then(response => {
-  console.log('previous uuid:', response.uuid);
+
     if(res) {
       User.findOneAndUpdate({uuid: response.uuid}, {uuid: uuid()}, {new: true}, function (err, res){
-        console.log(err, res);
+
         req.user.message('Logout sucessful.');
         next();
       })
