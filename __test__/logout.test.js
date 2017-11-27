ff0c7cde77f6a22f4e66a577b380518113a5189a
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-11-26 17:05:45 -0800

    logout test module complete

--- __test__/logout.test.js
+++ __test__/logout.test.js
@@ -1,10 +1,20 @@
 'use strict';
+
 const expect = require('expect');
 const User = require('../lib/user.js');
-const request = require('superagent');
 const app = require('../testServer/lib/server.js');
+
+//superagent bearer token
+const request = require('superagent');
+// request
+// .get('/some-url')
+// .set('Authorization', `Bearer ${token}`)
+
+//testing variables
 const username = 'username';
 const password = 'password';
+let jwtTest = '';
+
 
 describe('', done => {
   before (done => {
@@ -19,24 +29,68 @@ describe('', done => {
     done();
   });
 
-  it('Should get a mesage that jwt must be provided', done => {
-    request.get(`localhost:${process.env.PORT || 3000}/logout`).auth(username, password).then(response => {
-      expect(response.body.message).toEqual('jwt must be provided');
-      done();
-    });
-  });
-
-  it('Should send message that Logout sucessful', done => {
-    request.get(`localhost:${process.env.PORT || 3000}/signup`).auth(username, password)
-    request.get(`localhost:${process.env.PORT || 3000}/signin`).auth(username, password)
-    request.get(`localhost:${process.env.PORT || 3000}/logout`).auth(res.jwt)
-    request.get('password')
+  it('Should send a mesage that "No bearer token provided with Bearer. Please provide token."', done => {
+    request
+    .get(`localhost:${process.env.PORT || 3000}/logout`)
+    .set('Authorization', `Bearer `)
     .then(response => {
-      expect(response.body.message).toEqual('Logout sucessful.')
+      expect(response.body.message).toEqual("No bearer token provided with Bearer. Please provide token.");
       done();
     });
   });
 
+  it('Should send a mesage that "No auth header provided."', done => {
+    request
+    .get(`localhost:${process.env.PORT || 3000}/logout`)
+    .set('Authorization', ` `)
+    .then(response => {
+      expect(response.body.message).toEqual("No auth header provided.");
+      done();
+    });
+  });
 
+  it('Should send a mesage that Please provide a jwt token with the format: Bearer yourTokenHere', done => {
+    request
+    .get(`localhost:${process.env.PORT || 3000}/logout`)
+    .set('Authorization', 'Bearer undefined')
+    .then(response => {
+      expect(response.body.message).toEqual("Please provide a jwt token with the format: 'Bearer yourTokenHere'");
+      done();
+    });
+  });
 
+  //logout successful test
+  it('Should send message "Account Created" upon creation of new user', function(done) {
+        request.get(`localhost:${process.env.PORT || 3000}/signup`).auth(username, password).then(response => {
+                  // successful response
+         expect(response.body.message).toEqual('Account Created.');
+           done();
+        });
+      });
+  it('Should send message "Signed in successfully!" upon signin', function(done) {
+          request.get(`localhost:${process.env.PORT || 3000}/signin`).auth(username, password).then(response => {
+          console.log(response.body);
+          jwtTest = response.body.token;
+         expect(response.body.message).toEqual('Signed in successfully!');
+           done();
+        });
+      });
+  it('Should send message "Logout successful." upon logout', function(done) {
+    request
+    .get(`localhost:${process.env.PORT || 3000}/logout`)
+    .set('Authorization', `Bearer ${jwtTest}`)
+    .then(response => {
+         expect(response.body.message).toEqual('Logout successful.');
+           done();
+        });
+      });
+  it('Should send message "Unable to verify token."', function(done) {
+    request
+    .get(`localhost:${process.env.PORT || 3000}/logout`)
+    .set('Authorization', 'Bearer whatever')
+    .then(response => {
+         expect(response.body.message).toEqual("Unable to verify token.");
+           done();
+        });
+      });
 });
